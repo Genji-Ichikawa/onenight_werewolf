77c2afa5d1da63477ed06fa4c524767566a9199b
Genji-Ichikawa <genji_ichikawa@demia.co.jp>
2025-08-15 13:19:18 +0900

    Add player_for_poll.py

---
+++ player_for_poll.py
@@ -0,0 +1,32 @@
+from common import *
+
+import pygame
+
+BUTTON = Button(
+    font_file=JAPANESE_FONT_FILE,
+    font_size=FONT_SIZE_S,
+    x=BUTTON_X,
+    y=BUTTON_Y,
+    w=BUTTON_W,
+    h=BUTTON_H,
+)
+
+
+async def draw_player_for_poll_scene(player):
+    text_drawer = TextDrawer(JAPANESE_FONT_FILE)
+    text_drawer.draw_text_with_line_break(
+        text=f"{player['name']}にデバイスを渡してください。",
+        font_size=FONT_SIZE_M,
+        x=PLAYER_X,
+        y=PLAYER_Y,
+        w=PLAYER_W,
+    )
+    BUTTON.draw(f"{player['name']}です")
+
+
+async def update_player_for_poll_scene():
+    scene = SCENE_PLAYER_FOR_POLL
+    pos = pygame.mouse.get_pos()
+    if BUTTON.get_rect().collidepoint(pos):
+        scene = SCENE_POLL
+    return scene
